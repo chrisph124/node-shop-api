3b7979b5e4aafddc87fb1a9ce52a64510a64eff2
PHAM CHI Hieu <hieupc1@fsoft.com.vn>
2018-03-16 11:49:14 +0700

    Require morgan middleware

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const morgan = require('morgan');
 
 const app = express();
+
+// Only log error responses
+morgan('combined', {
+  skip(req, res) { return res.statusCode < 400; },
+});
 
 const products = require('./api/routes/products/index');
 const orders = require('./api/routes/orders/index');
 
+// Parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
+// Parse application/json
 app.use(bodyParser.json());
 
 app.use('/products', products);
